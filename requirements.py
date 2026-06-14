119abab2f8cdd84d1da115769f863a41096c8b68
saleknia <salekniaamirhossein@gmail.com>
2026-06-14 12:11:40 +0000

    Commit Process

---
+++ requirements.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cu124')
+os.system('pip install qudida')
+os.system('pip install monai==1.4 fvcore medpy')
