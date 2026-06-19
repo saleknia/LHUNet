c14494bb72901a1632af3c7ca8ad5173eceebf78
saleknia <salekniaamirhossein@gmail.com>
2026-06-19 15:10:59 +0000

    Commit Process

---
+++ scripts/NVDPancreas.sh
@@ -0,0 +1,4 @@
+export nnUNet_raw="/content/nnUNet_raw_data"
+export nnUNet_preprocessed="/content/nnUNet_preprocessed"
+export nnUNet_results="/content/nnUNet_results"
+nnUNetv2_plan_and_preprocess -d 802 -preprocessor_name IdentityPreprocessor --verify_dataset_integrity
